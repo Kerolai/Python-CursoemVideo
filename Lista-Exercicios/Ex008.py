2fbaad96b1af893e8e408ade75ca737e1376ea70
Kerolai Ribeiro <115428685+Kerolai@users.noreply.github.com>
2025-01-14 21:42:14 -0300

    Atualizando listas de exercícios
    Atualização dos exercícios

---
+++ Lista-Exercicios/Ex008.py
@@ -0,0 +1,8 @@
+#Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+metros = int(input('Entre com o valor em metros:'))
+
+centimetro = metros *20
+milimetro = centimetro*10
+
+print(f'O valor colocado em metros foi de {metros}, em centimetro é {centimetro} e em milimetro é {milimetro}')
