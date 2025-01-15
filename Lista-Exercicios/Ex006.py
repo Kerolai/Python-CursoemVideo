f32df39a7b53d91e5b920d9f10c404470a78f673
Kerolai Ribeiro <115428685+Kerolai@users.noreply.github.com>
2025-01-14 21:11:20 -0300

    Create Ex006.py
    Criado o EX006

---
+++ Lista-Exercicios/Ex006.py
@@ -0,0 +1,6 @@
+import math  
+# Importa a biblioteca math para usar a função sqrt
+#Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+n = int(input('Entre com um número: '))
+
+print(f'O dobro desse valor é {n*2}, o triplo desse valor é {n*3} e a raiz quadrada é {math.sqrt(n):.2f}')
